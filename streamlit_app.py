cf4a6395ee482604cee3ec6b504da2506a8ecbd6
rt4me <53063556+rt4me@users.noreply.github.com>
2023-04-05 10:08:51 -0500

    Update streamlit_app.py
    Adjust typo in insert statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,5 @@ streamlit.dataframe(my_data_rows)
 
 #Add area for person to enter fruit name to add to the list.
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-my_cur.execute("insert into fruit_load_list value ('from streamlit')")
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 streamlit.write('Thanks for adding ', add_my_fruit)
